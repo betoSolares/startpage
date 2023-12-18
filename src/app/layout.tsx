c6b009d0985137ec7c5584319a4267db75046288
Roberto Solares <betosolaresgar@gmail.com>
2023-12-18 17:47:51 -0600

    refactor: add UI basics
    Add tailwind and Geist font to the project. Also change the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 import type { Metadata } from 'next'
+import "@/styles/globals.css"
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+
 
 export const metadata: Metadata = {
   title: "Startpage",
   description: "Simple homepage for your browser",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
-      <body>
+    <html className={`${GeistSans.variable} ${GeistMono.variable}`} lang="en">
+      <body className='font-sans bg-neutral-100 dark:bg-neutral-900 min-h-screen min-w-full'>
           {children}
       </body>
     </html>
