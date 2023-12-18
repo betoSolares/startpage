cd6aa5f88c15c62aa43793b20d4380b449536f1c
Roberto Solares <betosolaresgar@gmail.com>
2023-12-18 17:47:51 -0600

    refactor: add UI basics
    Add tailwind and Geist font to the project. Also change the favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
 	return (
 	<div>
-			<p>Hello World!!!</p>
+			<p className="bg-red-500 font-sans">Hello World!!!</p>
+			<p className="bg-blue-500 font-mono">Mono TEXT</p>
 		</div>
 	)
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ['var(--font-geist-mono)', ...fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
