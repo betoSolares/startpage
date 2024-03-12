4c7ead32b28c4a8e28b0da0a50d8ccfb9909a8bb
Roberto Solares <betosolaresgar@gmail.com>
2024-03-12 15:15:38 -0600

    build: add user table

---
+++ prisma/migrations/20240312211419_auth_setup/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
