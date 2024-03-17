78a01ef0b1d573e5f1e29be00ca610250a14e8bc
Roberto Solares <betosolaresgar@gmail.com>
2024-03-17 12:07:34 -0600

    build: add next-auth dependencies

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,11 +7,13 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(['development', 'test', 'production'])
       .default('development'),
+    AUTH_SECRET: z.string(),
   },
   client: {},
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    AUTH_SECRET: process.env.AUTH_SECRET,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
