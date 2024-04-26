e2fd1e9521c4e47ac9d7dc6d2a99161828a7758d
Roberto Solares <betosolaresgar@gmail.com>
2024-04-25 19:19:05 -0600

    feat: add db helper to insert bookmarks

---
+++ src/server/data/bookmarks.ts
@@ -0,0 +1,21 @@
+import { BookmarkType } from '@prisma/client';
+import { fromPromise } from 'neverthrow';
+
+import { db } from '@/lib/db';
+
+import { PrismaError } from './error';
+
+export const createBookmark = async (
+  type: BookmarkType,
+  title: string,
+  userId: string,
+  link: string | undefined,
+  parentId: string | undefined
+) => {
+  const result = await fromPromise(
+    db.bookmark.create({ data: { type, title, userId, link, parentId } }),
+    (e) => new PrismaError(e)
+  );
+
+  return result;
+};
