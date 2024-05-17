f261f34457f1c23f904a0b839c97305d4a3269f6
Roberto Solares <betosolaresgar@gmail.com>
2024-05-16 18:48:25 -0600

    feat: transform link if it's a directory

--- src/schemas/bookmarks.ts
+++ src/schemas/bookmarks.ts
@@ -1,23 +1,43 @@
 import { BookmarkType } from '@prisma/client';
 import { z } from 'zod';
 
+const urlValidation = new RegExp(
+  '^(https?:\\/\\/)?' +
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+    '((\\d{1,3}\\.){3}\\d{1,3}))' +
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+    '(\\?[;&a-z\\d%_.~+=-]*)?' +
+    '(\\#[-a-z\\d_]*)?$',
+  'i'
+);
+
 export const CreateBookmarkSchema = z
   .object({
+    title: z.string().trim().min(1),
     type: z.nativeEnum(BookmarkType),
-    title: z.string(),
-    link: z.string().url().optional(),
+    link: z.string().optional(),
     parentId: z.string().cuid().optional(),
   })
   .refine(
     (values) => {
-      if (values.type === 'Link') {
-        return !!values.link;
+      if (
+        values.type === 'Link' &&
+        (values.link === undefined || !urlValidation.test(values.link))
+      ) {
+        return false;
       }
 
       return true;
     },
     {
-      message: 'Link is missing',
+      message: 'Invalid link',
       path: ['link'],
     }
-  );
+  )
+  .transform((values) => {
+    if (values.type === 'Directory') {
+      values.link = undefined;
+    }
+
+    return values;
+  });
