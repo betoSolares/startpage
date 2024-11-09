84ba6977f9306141a0a27ee7bee3878d42b20bcc
Roberto Solares <betosolaresgar@gmail.com>
2024-11-08 19:42:22 -0600

    feat: finish structure for settings page

--- src/app/settings/account/page.tsx
+++ src/app/settings/account/page.tsx
@@ -1,4 +1,4 @@
-import { Separator } from '@/app/_components/ui/separator';
+import { Button } from '@/app/_components/ui/button';
 import { auth } from '@/lib/auth';
 
 export default async function Page() {
@@ -6,47 +6,57 @@ export default async function Page() {
 
   return (
     <div className='space-y-6'>
-      <div>
+      <section className='flex flex-col gap-4'>
         <h3 className='scroll-m-20 text-2xl font-semibold tracking-tight'>
-          Account
+          Account Details
         </h3>
-        <p className='text-sm text-muted-foreground'>
-          Update your account settings.
-        </p>
-      </div>
-      <Separator />
-      <div className='flex flex-col gap-12'>
-        <section>
-          <h4 className='mb-4 scroll-m-20 text-xl font-semibold tracking-tight'>
-            Account Details
-          </h4>
-          <div className='flex flex-col gap-2'>
-            <section>
-              <p className='font-semibold'>Email address</p>
-              <p className='text-sm text-muted-foreground'>
+        <div className='divide-bg-border grid grid-flow-row divide-y'>
+          <section className='flex flex-row items-start justify-between gap-4 py-2'>
+            <div className='flex flex-grow flex-col'>
+              <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>
+                Email address
+              </h4>
+              <p className='leading-7 text-muted-foreground'>
                 {session?.user.email}
               </p>
-            </section>
-            <section>
-              <p className='font-semibold'>Password</p>
-              <p className='text-sm text-muted-foreground'>***********</p>
-            </section>
-          </div>
-        </section>
-        <section>
-          <h4 className='mb-4 scroll-m-20 text-xl font-semibold tracking-tight'>
-            Manage Account
-          </h4>
-          <div className='flex flex-col gap-2'>
-            <section>
-              <p className='font-semibold'>Delete account</p>
-              <p className='text-sm text-muted-foreground'>
+            </div>
+            <Button variant='link' size='lg' className='text-foreground'>
+              <p className='font-semibold'>Edit</p>
+            </Button>
+          </section>
+          <section className='flex flex-row items-start justify-between gap-4 py-2'>
+            <div className='flex flex-grow flex-col'>
+              <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>
+                Password
+              </h4>
+              <p className='leading-7 text-muted-foreground'>••••••••••••</p>
+            </div>
+            <Button variant='link' size='lg' className='text-foreground'>
+              <p className='font-semibold'>Edit</p>
+            </Button>
+          </section>
+        </div>
+      </section>
+      <section className='flex flex-col gap-4'>
+        <h3 className='scroll-m-20 text-2xl font-semibold tracking-tight'>
+          Manage Account
+        </h3>
+        <div className='divide-bg-border grid grid-flow-row divide-y'>
+          <section className='flex flex-row items-start justify-between gap-4 py-2'>
+            <div className='flex flex-grow flex-col'>
+              <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>
+                Delete Account
+              </h4>
+              <p className='leading-7 text-muted-foreground'>
                 Permanently delete your account.
               </p>
-            </section>
-          </div>
-        </section>
-      </div>
+            </div>
+            <Button variant='link' size='lg' className='text-destructive'>
+              <p className='font-semibold'>Delete</p>
+            </Button>
+          </section>
+        </div>
+      </section>
     </div>
   );
 }
--- src/app/settings/bookmarks/page.tsx
+++ src/app/settings/bookmarks/page.tsx
@@ -1,38 +1,41 @@
-import { Separator } from '@/app/_components/ui/separator';
+import { Button } from '@/app/_components/ui/button';
 
 export default async function Page() {
   return (
     <div className='space-y-6'>
-      <div>
+      <section className='flex flex-col gap-4'>
         <h3 className='scroll-m-20 text-2xl font-semibold tracking-tight'>
-          Bookmarks
+          Manage Bookmarks
         </h3>
-        <p className='text-sm text-muted-foreground'>
-          Manage your bookmarks collection.
-        </p>
-      </div>
-      <Separator />
-      <div className='flex flex-col gap-12'>
-        <section>
-          <h4 className='mb-4 scroll-m-20 text-xl font-semibold tracking-tight'>
-            Manage Bookmarks
-          </h4>
-          <div className='flex flex-col gap-2'>
-            <section>
-              <p className='font-semibold'>Import</p>
-              <p className='text-sm text-muted-foreground'>
-                Bring your bookmarks.
+        <div className='divide-bg-border grid grid-flow-row divide-y'>
+          <section className='flex flex-row items-start justify-between gap-4 py-2'>
+            <div className='flex flex-grow flex-col'>
+              <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>
+                Import Bookmarks
+              </h4>
+              <p className='leading-7 text-muted-foreground'>
+                Bring your bookmarks from other places.
               </p>
-            </section>
-            <section>
-              <p className='font-semibold'>Export</p>
-              <p className='text-sm text-muted-foreground'>
+            </div>
+            <Button variant='link' size='lg' className='text-foreground'>
+              <p className='font-semibold'>Import</p>
+            </Button>
+          </section>
+          <section className='flex flex-row items-start justify-between gap-4 py-2'>
+            <div className='flex flex-grow flex-col'>
+              <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>
+                Export Bookmarks
+              </h4>
+              <p className='leading-7 text-muted-foreground'>
                 Get a copy of your bookmarks.
               </p>
-            </section>
-          </div>
-        </section>
-      </div>
+            </div>
+            <Button variant='link' size='lg' className='text-foreground'>
+              <p className='font-semibold'>Export</p>
+            </Button>
+          </section>
+        </div>
+      </section>
     </div>
   );
 }
